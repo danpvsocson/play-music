10c1a2888b7c9724e9bad3265de640af567dc5bd
danpvsocson <ilovefamilyhk9@gmail.com>
2022-06-26 15:43:26 +0700

    update

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const imgSong = $('.cd-thumb')
 const audio = $('audio')
 const cd = $('.cd')
 const btnPlay = $('.btn-toggle-play')
+const btnNext = $('.btn-next')
+const btnPrev = $('.btn-prev')
+const btnRandom = $('.btn-random')
+const btnRepeat = $('.btn-repeat')
 const playIng = $('.player')
 const timeLoading = $('.progress')
 const app = {
+    random: false,
+    repeat: false,
     songIndex: 0,
     songs: [
         {
@@ -26,7 +32,7 @@ const app = {
         {
             name: 'Monody',
             singer: 'TheFatRat',
-            path: './assets/music/son3.mp3',
+            path: './assets/music/song3.mp3',
             image: './assets/img/song3.jpg'
         },
         {
@@ -48,25 +54,25 @@ const app = {
             image: './assets/img/song6.jpg'
         },
         {
-            name: 'Monody',
+            name: 'Monody 2',
             singer: 'TheFatRat',
             path: './assets/music/song3.mp3',
             image: './assets/img/song3.jpg'
         },
         {
-            name: 'Reality',
+            name: 'Reality 2',
             singer: 'Lost Frequencies',
             path: './assets/music/song4.mp3',
             image: '/assets/img/song4.jpg'
         },
         {
-            name: 'Vì Mẹ Bắt Chia Tay',
+            name: 'Vì Mẹ Bắt Chia Tay 2',
             singer: 'Miu Lê',
             path: './assets/music/song5.mp3',
             image: './assets/img/song5.jpg'
         },
         {
-            name: 'Dế Vương',
+            name: 'Dế Vương 2',
             singer: 'ACV',
             path: './assets/music/song6.mp3',
             image: './assets/img/song6.jpg'
@@ -97,14 +103,36 @@ const app = {
         })
     },
     xuLySuKien: function () {
+        function play () {
+            audio.onplay = function () {
+                checkPlayIng = true
+                playIng.classList.add('playing')
+                animate.play()
+            }
+            audio.onpause = function () {
+                checkPlayIng = false
+                playIng.classList.remove('playing')
+                animate.pause()
+            }
+            audio.ontimeupdate = function () {
+                if (audio.duration) {
+                    const time = Math.floor(audio.currentTime / audio.duration * 100)
+                    timeLoading.value = time
+                }
+            }
+            timeLoading.onchange = function (e) {
+                const seekTime = audio.duration / 100 * e.target.value
+                audio.currentTime = seekTime
+            }
+        }
         //* XỦ lý Img song
-        imgSong.animate([
-            { transition: 'rotate(360deg)' } //Quay 360
-        ]), {
+        const animate = imgSong.animate([
+            { transform: 'rotate(360deg)' } //Quay 360
+        ], {
             duration: 10000, // Quay trong 10s
             iterations: Infinity //Lặp vô hạn
-        }
-        // animate.pause()
+        })
+        animate.pause()
         // * Xử lý cuộn
         const cdWidth = cd.offsetWidth
         document.onscroll = function () {
@@ -121,24 +149,41 @@ const app = {
             } else {
                 audio.play()
             }
-            audio.onplay = function () {
-                checkPlayIng = true
-                playIng.classList.add('playing')
+            play()
+        }
+        // * Next song
+        btnNext.onclick = function () {
+            if(app.random){
+                app.playRandomSong()
+            }else{
+                app.nextSong()
             }
-            audio.onpause = function () {
-                checkPlayIng = false
-                playIng.classList.remove('playing')
+            audio.play()
+            play()
+        }
+        // * Prev Song
+        btnPrev.onclick = function () {
+            if(app.random){
+                app.playRandomSong()
+            }else{
+                app.prevSong()
             }
-            audio.ontimeupdate = function () {
-                if(audio.duration){
-                    const time = Math.floor(audio.currentTime / audio.duration * 100)
-                    timeLoading.value = time
-                }
+            audio.play()
+            play()
+            
+        }
+        // * Random Song
+        btnRandom.onclick = function () {
+            if(app.random){
+                btnRandom.classList.remove('active')
+                app.random = false
+            }else{
+                btnRandom.classList.add('active')
+                app.random = true
             }
-            timeLoading.onchange = function (e) {
-                const seekTime = audio.duration / 100 *  e.target.value
-                audio.currentTime = seekTime
-            }
+        }
+        btnRepeat.onclick = function () {
+
         }
     },
     loadSongNow: function () {
@@ -146,7 +191,28 @@ const app = {
         imgSong.style.backgroundImage = `url('${this.songNow.image}')`
         audio.src = this.songNow.path
     },
-
+    nextSong: function () {
+        this.songIndex++
+        if (this.songIndex >= this.songs.length) {
+            this.songIndex = 0
+        }
+        this.loadSongNow()
+    },
+    prevSong: function () {
+        this.songIndex--
+        if (this.songIndex < 0) {
+            this.songIndex = this.songs.length -1
+        }
+        this.loadSongNow()
+    },
+    playRandomSong: function () {
+        let newSongRandom
+        do {
+            newSongRandom = Math.floor(Math.random() * app.songs.length)
+        } while (newSongRandom === this.songIndex)
+        this.songIndex = newSongRandom
+        this.loadSongNow()
+    },
     start: function () {
         // ? Định nghĩa các thuộc tính
         this.defineProperties()
